de951e5e21aa5eb7cb78ff6a8e8b9f0bba90d2a1
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-03-14 01:01:28 +0900

    :whtie_check_mark: Rewrite in Pug

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<template lang="pug">
+img(alt="Vue logo", src="./assets/logo.png")
+HelloWorld(msg="Hello Vue 3 + TypeScript + Vite")
 </template>
 
 <style>
