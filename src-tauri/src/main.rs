05c1d140391165ecf08c95f93be009caafdc8f02
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-03-15 00:48:58 +0900

    :sparkles: Add logger

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,18 @@
   windows_subsystem = "windows"
 )]
 
+use tauri_plugin_log::{fern::colors::ColoredLevelConfig, LogTarget, LoggerBuilder};
+
 fn main() {
+  let targets = [LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview];
+  let colors = ColoredLevelConfig::default();
+  let logger = LoggerBuilder::new()
+    .targets(targets)
+    .with_colors(colors)
+    .build();
+
   tauri::Builder::default()
+    .plugin(logger)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { trace, info, error, attachConsole } from 'tauri-plugin-log-api'
 import HelloWorld from './components/HelloWorld.vue'
+
+// with LogTarget::Webview enabled this function will print logs to the browser console
+const detach = await attachConsole()
+
+trace('Trace')
+info('Info')
+error('Error')
+
+// detach the browser console from the log stream
+detach()
 </script>
 
 <template lang="pug">
